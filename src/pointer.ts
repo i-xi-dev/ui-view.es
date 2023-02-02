1f5e05fb38ce96cfc5bcff0a8b87f9342c4e0594
i-xi-dev <naoyuki.isono@gmail.com>
2023-02-02 23:41:17 +0900

    in development

--- src/pointer.ts
+++ src/pointer.ts
@@ -1,14 +1,16 @@
 
-type pointerid = number;
+// pointer lock に非対応
 
 // captureと単なるcontactは別にする （途中で変わった場合とか面倒すぎ）
+
+type pointerid = number;
 
 type _TargetGeometry = {
   boundingBox: {
     width: number,
     height: number,
-    viewportXOffset: number,
-    viewportYOffset: number,
+    offsetXFromViewport: number,
+    offsetYFromViewport: number,
   },
   viewport: {
     width: number,
@@ -18,11 +20,13 @@ type _TargetGeometry = {
   },
 };
 
-interface Pointer /*extends EventTarget*/ {
+interface Pointer {
+  get target(): Element;
   get pointerId(): pointerid;
   get pointerType(): string;
   get isPrimary(): boolean;
   get trackStream(): ReadableStream<Pointer.Track>;
+  [Symbol.asyncIterator](): AsyncGenerator<Pointer.Track, void, void>;//XXX ReadbleStream[Symbol.asyncIterator]がブラウザでなかなか実装されないので
 }
 
 class PointerImpl implements Pointer {
@@ -40,6 +44,10 @@ class PointerImpl implements Pointer {
     this.#trackStream = new ReadableStream(streamInit);
   }
 
+  get target(): Element {
+    return this.#target;
+  }
+
   get pointerId(): pointerid {
     return this.#pointerId;
   }
@@ -55,75 +63,86 @@ class PointerImpl implements Pointer {
   get trackStream(): ReadableStream<Pointer.Track> {
     return this.#trackStream;
   }
+
+  [Symbol.asyncIterator]() {
+    const streamReader = this.trackStream.getReader();
+    return (async function*() {
+      try {
+        for (
+          let i = await streamReader.read();
+          (i.done !== true);
+          i = await streamReader.read()
+        ) {
+          yield i.value;
+        }
+      } catch (exception) {
+        void exception; // XXX
+        return;
+      }
+      finally {
+        streamReader.releaseLock();
+      }
+    })();
+  }
 }
 
 namespace Pointer {
 
   export type Track = {
-    targetXOffset: number,
-    targetYOffset: number,
+    offsetXFromViewport: number,
+    offsetYFromViewport: number,
     timestamp: number,
     eventType: string,
-    targetMetrics?: _TargetGeometry,
-    intersectsTarget?: boolean,
+    offsetXFromTargetBoundingBox: number,
+    offsetYFromTargetBoundingBox: number,
+
+    _x_targetGeometry?: _TargetGeometry,//TODO いる？ 設定次第にすれば？
+    _x_intersectsTarget?: boolean,//TODO いる？ いるとしてもpointer側に持たせれば？
   };
 
   export type CaptureCallback = (pointer: Pointer) => void;
 
   export type CaptureOptions = {
-    captureNonPrimaryPointer?: boolean,
-    captureUntrustedPointer?: boolean,
+    filter?: (event: PointerEvent) => boolean, // pointerTypeでフィルタとか、isPrimaryでフィルタとか、isTrustedでフィルタとか、位置でフィルタとか、composedPath()でフィルタとか、いろいろできるようにevent自体を渡す
     maxConcurrentCaptures?: number,
-    //TODO pointerType制限
-    //TODO touch-actionでパンを無効にするか
+    highPrecision?: boolean,
+
+    //XXX touch-actionでパンを無効にするか
     //     子孫を強制pointer-events:noneにするか
-    //TODO pointermoveでgetCoalescedEventsも取得するか
+    //TODO pageに対しての座標をとるか
+    //TODO pointermoveでもelementsFromPointを取得するか
+    //TODO target他のresizeを監視するか
+    //TODO target,viewport他のscrollを監視するか
+    //TODO targetやviewport以外の任意の基準要素
   };
 
   export class CaptureTarget {
     readonly #target: Element;
-    readonly #captureNonPrimaryPointer: boolean;
-    readonly #captureUntrustedPointer: boolean;
+    readonly #filter: (event: PointerEvent) => boolean;
     readonly #maxConcurrentCaptures: number;
+    readonly #highPrecision: boolean;
     readonly #controllers: Map<pointerid, ReadableStreamDefaultController<Track>>;
     readonly #eventListenerAborter: AbortController;
     constructor(target: Element, callback: CaptureCallback, options: CaptureOptions = {}) {
       this.#target = target;
-      this.#captureNonPrimaryPointer = (options.captureNonPrimaryPointer === true);
-      this.#captureUntrustedPointer = (options.captureUntrustedPointer === true);
+      this.#filter = (typeof options.filter === "function") ? options.filter : () => true;
       this.#maxConcurrentCaptures = Number.isSafeInteger(options.maxConcurrentCaptures) ? (options.maxConcurrentCaptures as number) : 1;
+      this.#highPrecision = (options.highPrecision === true);
       this.#controllers = new Map();
       this.#eventListenerAborter = new AbortController();
       const { signal } = this.#eventListenerAborter;
 
       this.#target.addEventListener("pointerdown", ((event: PointerEvent) => {
-        if ((this.#captureNonPrimaryPointer !== true) && (event.isPrimary !== true)) {
-          return;
-        }
-        if ((this.#captureUntrustedPointer !== true) && (event.isTrusted !== true)) {
+        if (this.#filter(event) !== true) {
           return;
         }
         if (this.#controllers.size >= this.#maxConcurrentCaptures) {
           return;
         }
 
-        if (this.#target !== event.target) {
-          // pointerdownとgotpointercaptureでoffsetX/Yの基点が変わる場合に要注意
-          //    gotpointercaptureのtargetの子孫が、pointerdownのtargetの場合など
-          //    子孫はpointer-events:noneを推奨
-          console.error(`XXX 01`);
-          return;
-        }
         this.#target.setPointerCapture(event.pointerId);
 
-        const targetMetrics = this.#getTargetMetrics((event.target as Element), (event.view as Window));
-        const firstTrack = {
-          targetXOffset: event.offsetX,
-          targetYOffset: event.offsetY,
-          timestamp: event.timeStamp,
-          eventType: event.type,
-          targetMetrics,
-        };
+        const firstTrack = this.#getFirstTrack(event);
 
         const start = (controller: ReadableStreamDefaultController<Track>) => {
           this.#controllers.set(event.pointerId, controller);
@@ -144,17 +163,17 @@ namespace Pointer {
       // gotpointercapture
 
       this.#target.addEventListener("pointermove", ((event: PointerEvent): void => {
-        this.#pushTrack(event, false);
+        this.#pushTrack(event);
       }) as EventListener, { passive: true, signal });
 
       // pointerrawupdate
 
       this.#target.addEventListener("pointerup", ((event: PointerEvent): void => {
-        this.#pushTrack(event, true);
+        this.#pushLastTrack(event);
       }) as EventListener, { passive: true, signal });
 
       this.#target.addEventListener("pointercancel", ((event: PointerEvent): void => {
-        this.#pushTrack(event, true);
+        this.#pushLastTrack(event);
       }) as EventListener, { passive: true, signal });
 
       this.#target.addEventListener("lostpointercapture", ((event: PointerEvent): void => {
@@ -163,51 +182,104 @@ namespace Pointer {
 
     }
 
-    #getTargetMetrics(target: Element, view: Window) {
+    get #rootNode(): (Document | ShadowRoot) {
+      const root = this.#target.getRootNode();// コンストラクタで取得すると、まだconnectedされてないかも
+      if ((root instanceof Document) || (root instanceof ShadowRoot)) {
+        return root;
+      }
+      throw new Error("target is not connected");
+    }
+
+    #getTargetGeometry(target: Element, view: Window): _TargetGeometry {
       const targetBoundingBox = target.getBoundingClientRect();
       return {
         boundingBox: {
           width: targetBoundingBox.width,
           height: targetBoundingBox.height,
-          viewportXOffset: targetBoundingBox.left,
-          viewportYOffset: targetBoundingBox.top,
+          offsetXFromViewport: targetBoundingBox.left,
+          offsetYFromViewport: targetBoundingBox.top,
         },
         viewport: {
           width: view.innerWidth,
           height: view.innerHeight,
-          scrollX: view.screenX,
-          scrollY: view.screenY,
+          scrollX: view.scrollX,
+          scrollY: view.scrollY,
         },
       };
     }
 
-    #pushTrack(event: PointerEvent, terminated: boolean): void {
+    #getFirstTrack(event: PointerEvent): Track {
+      const targetGeometry = this.#getTargetGeometry((this.#target as Element), (event.view as Window));
+      let offsetXFromTargetBoundingBox = event.offsetX;
+      let offsetYFromTargetBoundingBox = event.offsetY;
+      if (this.#target !== event.target) {
+        const pointerdownTargetMetrics = this.#getTargetGeometry((event.target as Element), (event.view as Window));
+        offsetXFromTargetBoundingBox = offsetXFromTargetBoundingBox - (pointerdownTargetMetrics.boundingBox.offsetXFromViewport - targetGeometry.boundingBox.offsetXFromViewport);
+        offsetYFromTargetBoundingBox = offsetYFromTargetBoundingBox - (pointerdownTargetMetrics.boundingBox.offsetYFromViewport - targetGeometry.boundingBox.offsetYFromViewport);
+      }
+
+      return {
+        offsetXFromViewport: event.clientX,
+        offsetYFromViewport: event.clientY,
+        timestamp: event.timeStamp,
+        eventType: event.type,
+        offsetXFromTargetBoundingBox,
+        offsetYFromTargetBoundingBox,
+        _x_targetGeometry: targetGeometry,
+        _x_intersectsTarget: true,// falseはありえない
+      };
+    }
+
+    #pushTrack(event: PointerEvent): void {
       const controller = this.#controllers.get(event.pointerId);
       if (!!controller) {
-        if (terminated === true) {
-          const targetMetrics = this.#getTargetMetrics((event.target as Element), (event.view as Window));
-          const intersectsTarget = this.#target.ownerDocument.elementsFromPoint(event.clientX, event.clientY).includes(this.#target);//TODO shadowrootの場合
-          controller.enqueue({
-            targetXOffset: event.offsetX,
-            targetYOffset: event.offsetY,
-            timestamp: event.timeStamp,
-            eventType: event.type,
-            targetMetrics,
-            intersectsTarget,
-          });
-          controller.close();
+        if (this.#highPrecision === true) {
+          for (const coalesced of event.getCoalescedEvents()) {
+            controller.enqueue({
+              offsetXFromViewport: coalesced.clientX,
+              offsetYFromViewport: coalesced.clientY,
+              timestamp: coalesced.timeStamp,
+              eventType: coalesced.type,
+              offsetXFromTargetBoundingBox: coalesced.offsetX,
+              offsetYFromTargetBoundingBox: coalesced.offsetY,
+            });
+          }
         }
         else {
           controller.enqueue({
-            targetXOffset: event.offsetX,
-            targetYOffset: event.offsetY,
+            offsetXFromViewport: event.clientX,
+            offsetYFromViewport: event.clientY,
             timestamp: event.timeStamp,
             eventType: event.type,
+            offsetXFromTargetBoundingBox: event.offsetX,
+            offsetYFromTargetBoundingBox: event.offsetY,
           });
         }
       }
       else {
-        console.error(`XXX 02`);
+        // captureしてないpointer
+      }
+    }
+
+    #pushLastTrack(event: PointerEvent): void {
+      const controller = this.#controllers.get(event.pointerId);
+      if (!!controller) {
+        const targetGeometry = this.#getTargetGeometry((event.target as Element), (event.view as Window));
+        const intersectsTarget = this.#rootNode.elementsFromPoint(event.clientX, event.clientY).includes(this.#target);
+        controller.enqueue({
+          offsetXFromViewport: event.clientX,
+          offsetYFromViewport: event.clientY,
+          timestamp: event.timeStamp,
+          eventType: event.type,
+          offsetXFromTargetBoundingBox: event.offsetX,
+          offsetYFromTargetBoundingBox: event.offsetY,
+          _x_targetGeometry: targetGeometry,
+          _x_intersectsTarget: intersectsTarget,
+        });
+        controller.close();
+      }
+      else {
+        // captureしてないpointer
       }
     }
 
